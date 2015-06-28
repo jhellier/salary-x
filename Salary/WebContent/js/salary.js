d88ab3531c60790fab208bff4d61447a5e2b8a76
jhellier <qhellier@yahoo.com>
2015-06-27 21:28:17 -0600

    Added comments. Reformated code

--- Salary/WebContent/js/salary.js
+++ Salary/WebContent/js/salary.js
@@ -10,230 +10,245 @@
  * 
  * 
  */	
+
 	var salary = {
-			version: "1.0"
+		version : "1.0"
 	};
-	
-		var margin = {
-			top : 20,
-			right : 20,
-			bottom : 30,
-			left : 60
-		}, width = 960 - margin.left - margin.right, height = 300 - margin.top
-				- margin.bottom;
 
-		var y = d3.scale.ordinal().rangeRoundBands([0, height], .1);
+	var margin = {
+		top : 20,
+		right : 20,
+		bottom : 30,
+		left : 60
+	};
 
-		var x = d3.scale.linear().range([0, width]);
+	var width = 960 - margin.left - margin.right;
 
-		var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(20);
+	var height = 300 - margin.top - margin.bottom;
 
-		var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+	var y = d3.scale.ordinal().rangeRoundBands([ 0, height ], .1);
 
-		var svg = d3.select("#mainCompanyBar")
-					.append("svg")
-						.attr("width",	width + margin.left + margin.right + 60)
-						.attr("height",	height + margin.top + margin.bottom + 60)
-						.append("g")
-						.attr("transform",	"translate(" + margin.left + "," + margin.top + ")");
+	var x = d3.scale.linear().range([ 0, width ]);
+
+	var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(20);
+
+	var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+	var svg = d3.select("#mainCompanyBar").append("svg")
+					.attr("width",	width + margin.left + margin.right + 60)
+					.attr("height",	height + margin.top + margin.bottom + 60)
+				.append("g")
+					.attr("transform",	"translate(" + margin.left + "," + margin.top + ")");
 		
-		var data, barData;
+	var data;
 
-				
+	var barData;
+
+
+	/*
+	 * All the values computed here are not stored in the
+	 * DB because they are derived from values in the DB.
+	 */
+	function derivedValues() {
 		
-		d3.csv("data/10CompanyFinancials.csv", function(error, _data) {
-			data = _data.sort(function(a,b) {
-				return Number(b.Revenue) - Number(a.Revenue);
-			});
-			
-			computedValues();
-			buildMainBarGraph();
-		});
+		data = data.map(function(d) {
+			d.GrossProfit = (Number(d.Revenue) - Number(d.Cost));
+		    d.TaxesPaid = (Number(d.PreTaxIncome) - Number(d.Income));
+			d.RevenueEmp = (Number(d.Revenue)/Number(d.Employees)) * 1000;
+			d.GrossProfitEmp = (Number(d.GrossProfit)/Number(d.Employees)) * 1000;
+			d.AdminEmp = (Number(d.Admin)/Number(d.Employees)) * 1000;
+			d.RandDEmp = (Number(d.RandD)/Number(d.Employees)) * 1000;
+			d.OperatingEmp = (Number(d.Operating)/Number(d.Employees)) * 1000;
+			d.PreTaxIncomeEmp = (Number(d.PreTaxIncome)/Number(d.Employees)) * 1000;
+			d.IncomeEmp = (Number(d.Income)/Number(d.Employees)) * 1000;
+            d.TaxesPaidEmp = d.PreTaxIncomeEmp - d.IncomeEmp;
+            d.CostEmp = (Number(d.Cost)/Number(d.Employees)) * 1000;
+            if (d.IncomeEmp <= 0 || d.PreTaxIncomeEmp <= 0) {
+            	d.Salary100KEmp = 0;
+            } else {
+            	d.Salary100KEmp = (d.IncomeEmp/d.PreTaxIncomeEmp) * 100;	
+            }
+            d.Salary100K = (Number(d.Employees)/10);
+            if (d.Salary100K < 0) {
+            	d.Salary100K = 0;
+            }
+			return d;
+		})
+	}
+
+	/*
+	 * Builds the main set of horizontal bar graphs. This is the core of the viz and shows all
+	 * the different views for all the group of companies. Each view is a financial category such
+	 * as revenue or income or tax paid
+	 */
+	function buildMainBarGraph() {
+		
+		// Uses the list of companies to define the domain of the y axis.
+		// There are ten companies so there will be ten ticks on the y axis
+		y.domain(data.map(function(d) {
+			return d.Company;
+		}));
+		
+		// Derives the domain of the x axis from the company revenue. So the domain is from
+		// 0 to the max revenue of all the companies. The DB stores the values as millions
+		// We divide by 1000 to show the values as billions.
+		x.domain([ 0, d3.max(data, function(d) {
+			return Number(d.Revenue/1000);								
+		}) ]);
 		
 		
-		function computedValues() {
+		svg.append("g")
+			.attr("class", "y axis")
+			.attr("transform",	"translate("+ margin.left + ",0)")
+			.call(yAxis);
+
+		svg.append("g")
+				.attr("class", "x axis")
+				.attr("transform",	"translate("+ margin.left + "," + height  +   ")")
+			.call(xAxis)
+			.append("text")
+				.attr("x", width - 50)
+				.attr("y", 50)
+				.attr("dx",	"2em")
+				.attr("id","xAxisLabel")
+				.style("text-anchor", "end")
+				.text("Revenue (Billions)");
+		
+		// Uses .bar as the identifier. .bar does not have exist. It
+		// can be a made up name just to reserve a place for each element in the data array.
+		// In this case there is a css class defined as .bar but the selectAll could have
+		// been any identifier and it would still work. It is always best to apply an
+		// identifier to all appended elements because you have a distinct name to 
+		// query for. If you left it as "rect" then selectAll("rect") would get you 
+		// all the rects from a specific node in the DOM. 
+		var bars = svg.selectAll(".bar")
+				.data(data)
+			.enter().append("rect")
+				.attr("class", "bar")
+				.style("opacity", function(d,i) {
+					//Gives the candy cane effect on the rows
+					if (i % 2 == 0) {
+						return 0.5;
+					} else {
+						return 0.7;
+					}
+				})
+				.attr("y", function(d) {
+					return y(d.Company);
+				})
+				.attr("height", y.rangeBand())
+				.attr("x", function(d) {
+					return margin.left;
+				})
+				.attr("width", function(d) {
+					return x(d.Revenue/1000);
+				});
+		
+			svg.selectAll(".barText")
+			    .data(data)
+			   .enter().append("text")
+			    .attr("class", "barText")
+				.attr("text-anchor", "middle")
+				.attr("x",function(d) { return x(d.Revenue/1000);})
+				.attr("y", function(d) { return y(d.Company);})
+				.text(function(d) { return "";});
+		
+	};
+  				
+	/*
+	 *  Creates the vertical bar graphs. These are percentage views. There is one column that is
+	 *  the target and the rest are shown as what percentage of that target. For example, if revenue
+	 *  is the target and the other columns are cost and income then revenue is shown as 100% and 
+	 *  cost is shown as cost/revenue and income is shown as income/revenue.
+	 */		
+	function buildPercentageBarGraphs() {
+		
+            var svgBars = d3.select("#percentageCompanyBar").append("svg")
+                .attr("width",  width + margin.left + margin.right + 60)
+                .attr("height", height + 200)
+                .style("margin-left","50px")
+               .append("g")
+                .attr("transform",  "translate(" + margin.left + "," + margin.top + ")");
 			
-			data = data.map(function(d) {
-				d.GrossProfit = (Number(d.Revenue) - Number(d.Cost));
-			    d.TaxesPaid = (Number(d.PreTaxIncome) - Number(d.Income));
-				d.RevenueEmp = (Number(d.Revenue)/Number(d.Employees)) * 1000;
-				d.GrossProfitEmp = (Number(d.GrossProfit)/Number(d.Employees)) * 1000;
-				d.AdminEmp = (Number(d.Admin)/Number(d.Employees)) * 1000;
-				d.RandDEmp = (Number(d.RandD)/Number(d.Employees)) * 1000;
-				d.OperatingEmp = (Number(d.Operating)/Number(d.Employees)) * 1000;
-				d.PreTaxIncomeEmp = (Number(d.PreTaxIncome)/Number(d.Employees)) * 1000;
-				d.IncomeEmp = (Number(d.Income)/Number(d.Employees)) * 1000;
-                d.TaxesPaidEmp = d.PreTaxIncomeEmp - d.IncomeEmp;
-                d.CostEmp = (Number(d.Cost)/Number(d.Employees)) * 1000;
-                if (d.IncomeEmp <= 0 || d.PreTaxIncomeEmp <= 0) {
-                	d.Salary100KEmp = 0;
-                } else {
-                	d.Salary100KEmp = (d.IncomeEmp/d.PreTaxIncomeEmp) * 100;	
-                }
-                d.Salary100K = (Number(d.Employees)/10);
-                if (d.Salary100K < 0) {
-                	d.Salary100K = 0;
-                }
-				return d;
-			})
-		}
-						
-		function buildMainBarGraph() {
 			
-			y.domain(data.map(function(d) {
-				return d.Company;
-			}));
-			x.domain([ 0, d3.max(data, function(d) {
-				return Number(d.Revenue/1000);								
-			}) ]);
+			var lines = svgBars.append("line")
+			  .attr("x1",20)
+			  .attr("y1",120)
+			  .attr("x2",800)
+			  .attr("y2",120)
+			  .attr("stroke","black 2px solid");
 			
-			svg.append("g")
-				.attr("class", "y axis")
-				.attr("transform",	"translate("+ margin.left + ",0)")
-				.call(yAxis);
+            var gBars = svgBars.selectAll("g")
+                .data(barData.sort(function(a,b) {
+    				return Number(b.Revenue) - Number(a.Revenue);
+    			}))
+    			.enter().append("g");
 
-			svg.append("g")
-					.attr("class", "x axis")
-					.attr("transform",	"translate("+ margin.left + "," + height  +   ")")
-				.call(xAxis)
-				.append("text")
-					//.attr("transform",	"translate("+ width + margin.left + "," + height + ")")
-					.attr("x", width - 50)
-					.attr("y", 50)
-					.attr("dx",	"2em")
-					.attr("id","xAxisLabel")
-					.style("text-anchor", "end")
-					.text("Revenue (Billions)");
+            var columns = ["Revenue","Operating","PreTaxIncome","Income"];
+            
+            var color = d3.scale.category20();
+            
+            gBars.append("text")
+             .attr("text-anchor", "middle")
+                .attr("x", function(d,i) {
+                    if (i > 4) {
+                        return 40 + ((i - 5) * 180);
+                    } else {
+                        return 40 + (i * 180);  
+                    }
+                    
+                })
+                .attr("y", function(d,i) {
+                    if (i > 4) {
+                        return 320;
+                    } else {
+                        return 140;
+                    }
+                })
+                .text(function(d) { return d.Company;})
+             
 
-			var bars = svg.selectAll(".bar")
-					.data(data)
-    				.enter()
-    				.append("rect")
-					.attr("class", "bar")
-					.style("opacity", function(d,i) {
-						if (i % 2 == 0) {
-							return 0.5;
-						} else {
-							return 0.7;
-						}
-					})
-					.attr("y", function(d) {
-						return y(d.Company);
-					})
-					.attr("height", y.rangeBand())
-					.attr("x", function(d) {
-						return margin.left;
-					})
-					.attr("width", function(d) {
-						return x(d.Revenue/1000);
-					});
-			
-  				svg.selectAll(".barText")
-				    .data(data)
-				    .enter()
- 				    .append("text")
- 				    .attr("class", "barText")
-					.attr("text-anchor", "middle")
-					.attr("x",function(d) { return x(d.Revenue/1000);})
-					.attr("y", function(d) { return y(d.Company);})
-					.text(function(d) { return "";});
-			
+            for (var j = 0; j < columns.length; j++) {
+            	
+            	var startX = j * 10; 
+            	
+                gBars
+                .append("rect")
+                .attr("height", function(d) {                    
+                    var column = columns[j];
+                    if (d[column]/d.Revenue < 0)
+                        return 0;
+                    return 100 * (d[column]/d.Revenue);          
+                })
+                .attr("width",15)
+                .classed("masterCircle",true)
+                .attr("x", function(d,i) {
+                	if (i > 4) {
+                		return ((i - 5) * 200) + startX;  	                	      	                		
+                	} else {
+                		return (i * 200) + startX;
+                	}
+                })
+                .attr("y", function(d,i) {
+                    var column = columns[j];
+                    if (d[column]/d.Revenue < 0)
+                        return 0;
+                    var yPos = 100 * (d[column]/d.Revenue);          
 
-  				
-  				//Create vertical bar chart
-  				
-  				
-  				
-  	            var svgBars = d3.select("#percentageCompanyBar")
-  	            .append("svg")
-  	                .attr("width",  width + margin.left + margin.right + 60)
-  	                .attr("height", height + 200)
-  	                .style("margin-left","50px")
-  	                .append("g")
-  	                .attr("transform",  "translate(" + margin.left + "," + margin.top + ")");
-  				
-  				
-  				var lines = svgBars.append("line")
-  				  .attr("x0",20)
-  				  .attr("y0",120)
-  				  .attr("x1",800)
-  				  .attr("y1",120)
-  				  .attr("stroke","black 2px solid");
-  	            
-  				barData = data.slice();
-  				
-  	            var gBars = svgBars.selectAll("g")
-  	                .data(barData.sort(function(a,b) {
-	    				return Number(b.Revenue) - Number(a.Revenue);
-	    			}))
-  	                .enter()
-  	                .append("g");
-
-  	            var columns = ["Revenue","Operating","PreTaxIncome","Income"];
-  	            
-  	            var color = d3.scale.category20();
-  	            
-  	            gBars.append("text")
-  	             .attr("text-anchor", "middle")
-  	                .attr("x", function(d,i) {
-  	                    if (i > 4) {
-  	                        return 40 + ((i - 5) * 180);
-  	                    } else {
-  	                        return 40 + (i * 180);  
-  	                    }
-  	                    
-  	                })
-  	                .attr("y", function(d,i) {
-  	                    if (i > 4) {
-  	                        return 320;
-  	                    } else {
-  	                        return 140;
-  	                    }
-  	                })
-  	                .text(function(d) { return d.Company;})
-  	             
-
-  	            for (var j = 0; j < columns.length; j++) {
-  	            	
-  	            	var startX = j * 10; 
-  	            	
-  	                gBars
-  	                .append("rect")
-  	                .attr("height", function(d) {                    
-  	                    var column = columns[j];
-  	                    if (d[column]/d.Revenue < 0)
-  	                        return 0;
-  	                    return 100 * (d[column]/d.Revenue);          
-  	                })
-  	                .attr("width",15)
-  	                .classed("masterCircle",true)
-  	                .attr("x", function(d,i) {
-  	                	if (i > 4) {
-  	                		return ((i - 5) * 200) + startX;  	                	      	                		
-  	                	} else {
-  	                		return (i * 200) + startX;
-  	                	}
-  	                })
-  	                .attr("y", function(d,i) {
-                        var column = columns[j];
-                        if (d[column]/d.Revenue < 0)
-                            return 0;
-                        var yPos = 100 * (d[column]/d.Revenue);          
-
-  	                	
-  	                    if (i > 4) {
-  	                    	return 300 - yPos;  	                        
-  	                    } else {
-  	                    	return 120 - yPos;
-  	                    }
-  	                })
-  	                .style("fill", function(d) {
-  	                    return color(columns[j]);
-  	                });
-  	                
-  	            }
-  	       
-		};  				
+                	
+                    if (i > 4) {
+                    	return 300 - yPos;  	                        
+                    } else {
+                    	return 120 - yPos;
+                    }
+                })
+                .style("fill", function(d) {
+                    return color(columns[j]);
+                });
+                
+            }
+       
+	};  				
 					
         
 		function sortBy(sortColumn, label, isCost) {
@@ -440,7 +455,22 @@
 		  d3.select("#sortIncomeEmployee").on("click", function() { return sortByPerEmployee("IncomeEmp", "Income Per Employee (Thousands)", false); });
 		  d3.select("#sort100KEmployee").on("click", function() { return sortByPerEmployee("Salary100KEmp", "Cost of 100K Salary Increase For Each Employee (Thousands)", false); });
 		  
-		  //This makes salary available to the world. amd is Asychronous Module Definition. When using the bang function convention
+		  
+  		d3.csv("data/10CompanyFinancials.csv", function(error, _data) {
+				data = _data.sort(function(a,b) {
+					return Number(b.Revenue) - Number(a.Revenue);
+				});
+				
+				//Copy the data to barData so that it can be changed independently
+				barData = data.slice();
+				
+				derivedValues();
+				buildMainBarGraph();
+				buildPercentageBarGraphs();
+			});
+
+		  
+		  //This makes salary available to the world. amd is Asynchronous Module Definition. When using the bang function convention
 		  //to define this module you need to put the following block to to expose salary to the world.
 		  if (typeof define === "function" && define.amd) 
 			  define(salary); 
